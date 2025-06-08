9baa3abc65999fc126210e40a9186a619acdba3e
joao1276 <sintrageysy@gmail.com>
2025-06-08 16:38:52 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+name: painel_unlock_mi_account
+description: Painel de desbloqueio Xiaomi estilo oficial.
+version: 1.0.0+1
+environment:
+  sdk: ">=2.17.0 <3.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+flutter:
+  uses-material-design: true
+    
